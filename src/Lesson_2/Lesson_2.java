9161c38ae6b3a65758a04c07002d03b533837d76
github@oneclickstudio.xyz <123456789Git>
2018-09-06 11:40:54 +0400

    Lesson 2

---
+++ src/Lesson_2/Lesson_2.java
@@ -0,0 +1,22 @@
+package Lesson_2;
+
+public class Lesson_2 {
+    public static void main(String[] args) {
+        char grade = 'D';
+
+            switch (grade) {
+                case 'A' :
+                    System.out.println('A');
+                case 'B' :
+                    System.out.println('B');
+                case 'C' :
+                    System.out.println('C');
+                default:
+                    System.out.println('Z');
+                case 'X' :
+                    System.out.println('X');
+                case 'Y' :
+                    System.out.println('Y');
+            }
+    }
+}
